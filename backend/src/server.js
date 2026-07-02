637e55f4294c330b2cb8377af5a4aa94cdc74e04
sksameed <sksameed999@gmail.com>
2026-07-02 20:08:31 +0530

    catalog

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,7 @@ const __dirname = path.dirname(__filename);
 
 
 dotenv.config();
+console.log("BASE_URL:", process.env.BASE_URL);
 console.log("Gemini Key Loaded:", !!process.env.GEMINI_API_KEY);
 const app = express();
 
